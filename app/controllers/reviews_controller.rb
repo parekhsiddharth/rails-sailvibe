ba21534499d84203c9fd85cfd92ff45bd83f89ca
Sony Tamang <sony123ktm@gmail.com>
2022-03-02 21:26:56 +1100

    Add review features with validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,18 +5,19 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
     if @review.valid?
       @review.save
       redirect_to yacht_path(id: @review.yacht_id)
     else
       @review = Review.new
-      render 'yacht/show'
+      redirect_back(fallback_location: root_path)
     end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:rating, :content, :yacht_id, :user_id)
+    params.require(:review).permit(:rating, :comment, :yacht_id, :user_id)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :yacht
   belongs_to :user
+  validates :rating, inclusion: 0..5
+  validates :rating, numericality: { only_integer: true }
 end
