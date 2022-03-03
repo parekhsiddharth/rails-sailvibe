d5fe2cd3297aba41c2b1578c61a58e0257e376b1
Ervina Theresa <ervina.theresa@gmail.com>
2022-03-03 12:20:38 +1100

    Add dotenv - Protect my secret data in .env file

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -6,4 +6,7 @@ class Yacht < ApplicationRecord
 
   validates :name, :price, presence: true
   has_many_attached :photos
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
