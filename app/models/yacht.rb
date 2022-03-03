8aff7e34aafb9f3d11fbd3ee0880700f2f31b7a8
Sony Tamang <sony123ktm@gmail.com>
2022-03-03 11:59:02 +1100

    Added PG gem and basic search feature

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -6,4 +6,11 @@ class Yacht < ApplicationRecord
 
   validates :name, :price, presence: true
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :location, :description, :price],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
