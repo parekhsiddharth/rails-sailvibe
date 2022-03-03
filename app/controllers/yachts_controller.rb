129dde62d1be13eb900a18d1205afc58ed811f19
Siddharth Parekh <parekh.siddharth@gmail.com>
2022-03-03 15:24:00 +1100

    updated the user and owner actions of edit/update and delete bookings/listings

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,5 +1,6 @@
 class YachtsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :find_yacht, only: %i[edit update destroy]
 
   def index
     @yachts = Yacht.all
@@ -25,9 +26,44 @@ class YachtsController < ApplicationController
     end
   end
 
+  def my_yachts
+    @user = current_user
+    @my_yachts = @user.yachts
+  end
+
+  # def edit
+  #   @user = current_user
+  #   @my_yachts = @user.yachts[:id]
+  # end
+
+  def edit
+    # @yacht.user_id = current_user.id
+    # @my_yacht = Yacht.find(params[:id])
+  end
+
+  def update
+    @my_yacht = Yacht.find(params[:id])
+    # @my_yacht.user_id = current_user.id
+    if @my_yacht.update(yacht_params)
+      redirect_to my_yachts_yachts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @my_yacht = Yacht.find(params[:id])
+    @my_yacht.destroy
+    redirect_to my_yachts_yachts_path
+  end
+
   private
 
   def yacht_params
     params.require(:yacht).permit(:name, :price, :description, photos: [])
+  end
+
+  def find_yacht
+    @my_yacht = Yacht.find(params[:id])
   end
 end
